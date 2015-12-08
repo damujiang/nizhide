98487bda313c4c4ac88c8a796dfc11bdac05dfd8
MU JIANG <1819021@qq.com>
2015-12-08 15:42:17 +0800

    add logback

--- nizhide-ui/src/main/java/com/nizhide/ui/controller/ArticleController.java
+++ nizhide-ui/src/main/java/com/nizhide/ui/controller/ArticleController.java
@@ -3,11 +3,14 @@ package com.nizhide.ui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;;
 
 @Controller
 public class ArticleController {
 	
+	 private static final Logger LOGGER = LoggerFactory.getLogger(ArticleController.class);
+
 	@RequestMapping("/article")
 	 public String article(){
 		 return "article";
@@ -15,6 +18,7 @@ public class ArticleController {
 
 	@RequestMapping("/article2")
 	 public String article2(){
+		LOGGER.info("ENTER INTO article2");
 		 return "article2";
 	}
 	
